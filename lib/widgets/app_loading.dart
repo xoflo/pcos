c38e136a904582d31b1ea5eec6f94be4e07f2c16
Andy Frost <andyfrost50@hotmail.com>
2020-12-07 14:57:57 +1300

    Added splash screen for Android and iOS. Might need to update background colors later to correctly match PCOS colours.

--- lib/widgets/app_loading.dart
+++ lib/widgets/app_loading.dart
@@ -3,8 +3,23 @@ import 'package:flutter/material.dart';
 class AppLoading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: CircularProgressIndicator(),
+    return MaterialApp(
+      title: 'The PCOS Protocol',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        backgroundColor: Colors.orangeAccent.shade200,
+        appBar: AppBar(
+          title: Text("App Title"),
+        ),
+        body: Center(
+          child: CircularProgressIndicator(
+            backgroundColor: Colors.redAccent,
+          ),
+        ),
+      ),
     );
   }
 }
