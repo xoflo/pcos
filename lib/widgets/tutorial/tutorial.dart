e5375da685d372307889cd9280948e5d3258fc9d
Andy Frost <andyfrost50@hotmail.com>
2021-03-02 17:06:15 +1300

    Made tutorial full screen with dots, added dependency for Health kit. Also updated firebase-crashlytics version to latest.

--- lib/widgets/tutorial/tutorial.dart
+++ lib/widgets/tutorial/tutorial.dart
@@ -1,17 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:thepcosprotocol_app/styles/colors.dart';
+import 'package:thepcosprotocol_app/widgets/shared/color_button.dart';
 import 'package:thepcosprotocol_app/widgets/shared/dialog_header.dart';
 import 'package:thepcosprotocol_app/constants/favourite_type.dart';
 import 'package:thepcosprotocol_app/utils/device_utils.dart';
 import 'package:thepcosprotocol_app/generated/l10n.dart';
 
-class Tutorial extends StatelessWidget {
+class Tutorial extends StatefulWidget {
   final Function closeTutorial;
 
   Tutorial({this.closeTutorial});
 
+  @override
+  _TutorialState createState() => _TutorialState();
+}
+
+class _TutorialState extends State<Tutorial> {
+  //final CarouselController _carouselController = CarouselController();
+  int _currentPage = 0;
+
   double _getTabBarHeight(BuildContext context) {
     return MediaQuery.of(context).size.height - (kToolbarHeight + 20);
+  }
+
+  Widget _getCarouselPager(final BuildContext context, final int totalPages) {
+    List<Widget> circleList = new List<Widget>();
+    for (var i = 0; i < totalPages; i++) {
+      circleList.add(
+        _drawCircle(
+          i == _currentPage ? primaryColorDark : primaryColorLight,
+          i == _currentPage ? 14 : 10,
+        ),
+      );
+    }
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: circleList,
+    );
+  }
+
+  Widget _drawCircle(final Color color, final double size) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 8.0,
+        right: 8.0,
+        top: 6.0,
+        bottom: 2.0,
+      ),
+      child: SizedBox(
+        width: size,
+        height: size,
+        child: CircleAvatar(
+          backgroundColor: color,
+        ),
+      ),
+    );
   }
 
   @override
@@ -20,34 +65,31 @@ class Tutorial extends StatelessWidget {
     final isHorizontal =
         DeviceUtils.isHorizontalWideScreen(screenSize.width, screenSize.height);
 
-    return Container(
-      height: _getTabBarHeight(context),
-      child: Card(
+    return Padding(
+      padding: const EdgeInsets.only(top: 30.0),
+      child: SizedBox.expand(
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              DialogHeader(
-                screenSize: screenSize,
-                favouriteType: FavouriteType.None,
-                title: S.of(context).tutorialTitle,
-                isFavourite: false,
-                closeItem: closeTutorial,
-              ),
               CarouselSlider(
                 options: CarouselOptions(
-                  height: DeviceUtils.getRemainingHeight(
-                      screenSize.height, false, isHorizontal, false, false),
+                  height: screenSize.height - 100,
                   enableInfiniteScroll: false,
-                  viewportFraction: 0.9,
+                  viewportFraction: 1,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      _currentPage = index;
+                    });
+                  },
                 ),
                 items: [1, 2, 3, 4, 5].map((i) {
                   return Builder(
                     builder: (BuildContext context) {
                       return Container(
                           width: MediaQuery.of(context).size.width,
-                          margin: EdgeInsets.symmetric(horizontal: 5.0),
-                          decoration: BoxDecoration(color: Colors.amber),
+                          margin: EdgeInsets.symmetric(horizontal: 0),
+                          decoration: BoxDecoration(color: Colors.white),
                           child: Text(
                             'text $i',
                             style: TextStyle(fontSize: 16.0),
@@ -55,7 +97,15 @@ class Tutorial extends StatelessWidget {
                     },
                   );
                 }).toList(),
-              )
+              ),
+              _getCarouselPager(context, 5),
+              ColorButton(
+                isUpdating: false,
+                label: "Close",
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
             ],
           ),
         ),
