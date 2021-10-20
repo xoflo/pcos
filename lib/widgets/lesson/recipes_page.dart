336241d1fa1dc00a9c93984e55eec45b1bd55b52
Andy Frost <andyfrost50@hotmail.com>
2021-10-21 12:45:03 +1300

    Added recipe page, needs layout.

---
+++ lib/widgets/lesson/recipes_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:thepcosprotocol_app/styles/colors.dart';
+import 'package:thepcosprotocol_app/constants/favourite_type.dart';
+import 'package:thepcosprotocol_app/models/lesson_recipe.dart';
+import 'package:thepcosprotocol_app/generated/l10n.dart';
+
+class RecipesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
