800008563765de15b6e7a60912533310cc178fc0
Jacob Chan <jacob@gitlab.puttiapps.com>
2022-07-29 08:46:24 +0800

    Remove betterPlayer in the video component
    This way, we avoid loading the video
    details and not show the number of
    minutes. This improves performance,
    especially when there are multiple
    videos in one lesson content item

--- lib/widgets/shared/video_component.dart
+++ lib/widgets/shared/video_component.dart
@@ -1,4 +1,3 @@
-import 'package:better_player/better_player.dart';
 import 'package:flutter/material.dart';
 import 'package:thepcosprotocol_app/styles/colors.dart';
 import 'package:thepcosprotocol_app/widgets/lesson/lesson_video_page.dart';
@@ -13,143 +12,42 @@ class VideoComponent extends StatefulWidget {
 }
 
 class _VideoComponentState extends State<VideoComponent> {
-  late BetterPlayerController _betterPlayerController;
-
-  int? minutes;
-
-  bool isVideoAvailable = false;
-
   @override
   void initState() {
     super.initState();
-
-    if (widget.videoUrl.isNotEmpty) {
-      BetterPlayerDataSource betterPlayerDataSource = BetterPlayerDataSource(
-          BetterPlayerDataSourceType.network, widget.videoUrl);
-
-      BetterPlayerConfiguration betterPlayerConfiguration =
-          BetterPlayerConfiguration(
-        autoPlay: false,
-        autoDispose: true,
-        looping: false,
-        fullScreenByDefault: false,
-      );
-
-      _betterPlayerController = BetterPlayerController(
-        betterPlayerConfiguration,
-        betterPlayerDataSource: betterPlayerDataSource,
-      );
-
-      _betterPlayerController.addEventsListener(_setDuration);
-    } else if (mounted) {
-      setState(() => isVideoAvailable = false);
-    }
-  }
-
-  void _setDuration(BetterPlayerEvent event) {
-    final duration = event.parameters?['duration'] as Duration?;
-    if (duration != null && mounted) {
-      setState(() {
-        isVideoAvailable = true;
-        minutes = duration.inMinutes;
-      });
-    }
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _betterPlayerController.pause();
-    _betterPlayerController.dispose(forceDispose: true);
-    _betterPlayerController.removeEventsListener(_setDuration);
   }
 
   @override
   Widget build(BuildContext context) => GestureDetector(
-        onTap: isVideoAvailable
-            ? () => Navigator.pushNamed(
-                  context,
-                  LessonVideoPage.id,
-                  arguments: widget.videoUrl,
-                )
-            : null,
-        child: Opacity(
-          opacity: isVideoAvailable ? 1 : 0.5,
-          child: Container(
-            width: double.maxFinite,
-            padding: EdgeInsets.all(25),
-            decoration: BoxDecoration(
-              color: primaryColorLight,
-              borderRadius: BorderRadius.all(Radius.circular(16)),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Icon(
-                      Icons.play_circle,
-                      color: backgroundColor,
-                      size: 25,
-                    ),
-                    SizedBox(width: 10),
-                    Text(
-                      "Watch Video",
-                      style: Theme.of(context)
-                          .textTheme
-                          .headline5
-                          ?.copyWith(color: backgroundColor),
-                    ),
-                  ],
+        onTap: () => Navigator.pushNamed(
+          context,
+          LessonVideoPage.id,
+          arguments: widget.videoUrl,
+        ),
+        child: Container(
+          width: double.maxFinite,
+          padding: EdgeInsets.all(25),
+          decoration: BoxDecoration(
+            color: primaryColorLight,
+            borderRadius: BorderRadius.all(Radius.circular(16)),
+          ),
+          child: Row(
+            children: [
+              Icon(
+                Icons.play_circle,
+                color: backgroundColor,
+                size: 25,
+              ),
+              SizedBox(width: 10),
+              Text(
+                "Watch Video",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: backgroundColor,
                 ),
-                if (isVideoAvailable) ...[
-                  SizedBox(height: 15),
-                  Row(
-                    children: [
-                      Row(
-                        children: [
-                          Image(
-                            image: AssetImage('assets/lesson_video.png'),
-                            width: 20,
-                            height: 20,
-                          ),
-                          SizedBox(width: 5),
-                          Text(
-                            "Video",
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyText2
-                                ?.copyWith(
-                                    fontWeight: FontWeight.normal,
-                                    color: textColor.withOpacity(0.8)),
-                          ),
-                        ],
-                      ),
-                      SizedBox(width: 15),
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.schedule,
-                            size: 20,
-                            color: textColor.withOpacity(0.8),
-                          ),
-                          SizedBox(width: 5),
-                          Text(
-                            "$minutes mins",
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyText2
-                                ?.copyWith(
-                                    fontWeight: FontWeight.normal,
-                                    color: textColor.withOpacity(0.8)),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                ]
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       );
