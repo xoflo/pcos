a09f2915d09a5ef0b7c10f1e5f066e09ad13f142
Vergil Santos <vergilsantos@Deployeds-MacBook-Pro.local>
2022-08-01 08:32:00 +0800

    delete unused files (use dart_code_metrics to determine unused files)

--- lib/globals.dart
+++
@@ -1,7 +0,0 @@
-import 'package:firebase_analytics/firebase_analytics.dart';
-
-class Global {
-  static final FirebaseAnalytics analytics = FirebaseAnalytics.instance;
-  static final FirebaseAnalyticsObserver observer =
-      FirebaseAnalyticsObserver(analytics: analytics);
-}
--- lib/models/chat_message.dart
+++
@@ -1,7 +0,0 @@
-class ChatMessage {
-  final String? messageText;
-  final bool? isUser;
-  //final dateCreatedUTC;
-
-  ChatMessage({this.messageText, this.isUser});
-}
