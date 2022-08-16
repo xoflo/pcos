96aa34cd35f3b4880cee3fa5c80516a9aceb6b1a
Jacob Chan <jacob@gitlab.puttiapps.com>
2022-08-16 13:30:39 +0800

    Fix why loading issue when there is no actual text

--- lib/screens/tabs/dashboard/dashboard_why_community.dart
+++ lib/screens/tabs/dashboard/dashboard_why_community.dart
@@ -1,93 +1,94 @@
 import 'package:flutter/material.dart';
 import 'package:animations/animations.dart';
 import 'package:provider/provider.dart';
+import 'package:thepcosprotocol_app/constants/loading_status.dart';
 import 'package:thepcosprotocol_app/providers/member_provider.dart';
 import 'package:thepcosprotocol_app/styles/colors.dart';
 import 'package:thepcosprotocol_app/screens/tabs/dashboard/dashboard_why_settings_page.dart';
+import 'package:thepcosprotocol_app/widgets/shared/pcos_loading_spinner.dart';
 
 class DashboardWhyCommunity extends StatelessWidget {
-  const DashboardWhyCommunity({Key? key})
-      : super(key: key);
+  const DashboardWhyCommunity({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-          padding: EdgeInsets.symmetric(horizontal: 25),
-          child: Row(
-            children: [
-              Expanded(
-                child: Consumer<MemberProvider>(
-                  builder: (context, memberViewModel, _) => 
-                  OpenContainer(
-                    transitionDuration: Duration(milliseconds: 400),
-                    clipBehavior: Clip.hardEdge,
-                    openBuilder: (context, closedContainer) {
-                        return DashboardWhySettingsPage();
-                    },
-                    openShape: RoundedRectangleBorder(
+  Widget build(BuildContext context) => Container(
+        padding: EdgeInsets.symmetric(horizontal: 25),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Consumer<MemberProvider>(
+                builder: (context, memberProvider, _) => OpenContainer(
+                  transitionDuration: Duration(milliseconds: 400),
+                  clipBehavior: Clip.hardEdge,
+                  openBuilder: (context, closedContainer) =>
+                      DashboardWhySettingsPage(),
+                  openShape: RoundedRectangleBorder(
                       side: BorderSide(color: tertiaryColor, width: 0),
                       borderRadius: BorderRadius.circular(16)),
-                    closedShape: RoundedRectangleBorder(
+                  closedShape: RoundedRectangleBorder(
                       side: BorderSide(color: tertiaryColor, width: 0),
                       borderRadius: BorderRadius.circular(16)),
-                    openColor: tertiaryColor,
-                    middleColor: tertiaryColor,
-                    closedColor: tertiaryColor,
-                    closedElevation: 0,
-                    closedBuilder: (context, openContainer) {
-                      return Container(
-                            height: 125,
-                            alignment: Alignment.center,
-                            padding: EdgeInsets.all(10),
-                            child: Text(
-                              memberViewModel.why.isNotEmpty
-                                  ? memberViewModel.why
-                                  : "Please input your why to motivate yourself every day.",
-                              style: Theme.of(context).textTheme.headline5!
-                            ),
-                      );
-                    },
+                  openColor: tertiaryColor,
+                  middleColor: tertiaryColor,
+                  closedColor: tertiaryColor,
+                  closedElevation: 0,
+                  closedBuilder: (context, openContainer) => Container(
+                    height: 125,
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(12.5),
+                    child: memberProvider.status == LoadingStatus.loading
+                        ? Padding(
+                            padding: EdgeInsets.only(bottom: 30),
+                            child: PcosLoadingSpinner(),
+                          )
+                        : Text(
+                            !memberProvider.why.isNotEmpty
+                                ? memberProvider.why
+                                : "Please input your why to motivate yourself every day.",
+                            softWrap: false,
+                            maxLines: 4,
+                            overflow: TextOverflow.ellipsis,
+                            style: Theme.of(context).textTheme.headline5!),
                   ),
                 ),
               ),
-              SizedBox(width: 5),
-              Expanded(
-                child: Container(
-                  height: 125,
-                  child: Card(
-                    elevation: 0,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16),
-                    ),
-                    color: backgroundColor,
-                    child: GestureDetector(
-                      child: Container(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Icon(Icons.group_outlined),
-                            SizedBox(height: 5),
-                            Text(
-                              "Open community",
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .subtitle1
-                                  ?.copyWith(
-                                    color: Colors.white,
-                                    fontSize: 20,
-                                  ),
-                            )
-                          ],
-                        ),
+            ),
+            SizedBox(width: 5),
+            Expanded(
+              child: Container(
+                height: 125,
+                child: Card(
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  color: backgroundColor,
+                  child: GestureDetector(
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Icon(Icons.group_outlined),
+                          SizedBox(height: 5),
+                          Text(
+                            "Open community",
+                            style:
+                                Theme.of(context).textTheme.subtitle1?.copyWith(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                    ),
+                          )
+                        ],
                       ),
                     ),
                   ),
                 ),
               ),
-            ],
-          ),
-        );
-  }
+            ),
+          ],
+        ),
+      );
 }
