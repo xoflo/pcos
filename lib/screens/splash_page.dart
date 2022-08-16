77675a87a9e004d6e330e57daa364bfe8efc617d
Jacob Chan <jacob@gitlab.puttiapps.com>
2022-08-16 15:10:50 +0800

    Add splash page

--- lib/screens/splash_page.dart
+++ lib/screens/splash_page.dart
@@ -6,21 +6,26 @@ import 'package:thepcosprotocol_app/controllers/authentication_controller.dart';
 import 'package:thepcosprotocol_app/styles/colors.dart';
 import 'package:thepcosprotocol_app/utils/device_utils.dart';
 import 'package:thepcosprotocol_app/screens/onboarding/onboarding_page.dart';
+import 'package:thepcosprotocol_app/widgets/shared/loader_overlay.dart';
 
-class AppLoading extends StatefulWidget {
-  static const String id = "app_loading_screen";
+class SplashPage extends StatefulWidget {
+  static const String id = "splash_page";
 
   @override
-  _AppLoadingState createState() => _AppLoadingState();
+  _SplashPageState createState() => _SplashPageState();
 }
 
-class _AppLoadingState extends State<AppLoading> {
+class _SplashPageState extends State<SplashPage> {
   bool _isLocked = false;
+  bool isLoading = false;
 
   @override
   void initState() {
     super.initState();
-    _appOpening();
+    Future.delayed(Duration(seconds: 2), () {
+      setState(() => isLoading = true);
+      _appOpening();
+    });
   }
 
   Future<void> _appOpening() async {
@@ -51,15 +56,19 @@ class _AppLoadingState extends State<AppLoading> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: primaryColor,
-      body: Center(
-        child: CircularProgressIndicator(
-          backgroundColor: backgroundColor,
-          valueColor: new AlwaysStoppedAnimation<Color>(primaryColor),
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: backgroundColor,
+        body: Stack(
+          children: [
+            Center(
+              child: Image.asset(
+                "assets/logo_orange.png",
+                height: 50,
+                width: 125,
+              ),
+            ),
+            if (isLoading) LoaderOverlay()
+          ],
         ),
-      ),
-    );
-  }
+      );
 }
