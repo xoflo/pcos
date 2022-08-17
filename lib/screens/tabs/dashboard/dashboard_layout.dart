91d1d87249c4837c6b995073a922a29d067888d7
Vergil Santos <vergil@puttiapps.com>
2022-08-17 14:39:23 +0800

    remove commented out codes

--- lib/screens/tabs/dashboard/dashboard_layout.dart
+++ lib/screens/tabs/dashboard/dashboard_layout.dart
@@ -12,42 +12,8 @@ final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
 class DashboardLayout extends StatelessWidget {
   DashboardLayout({Key? key}) : super(key: key);
 
-  // final bool showYourWhy;
-  // final bool showLessonReicpes;
-  // final String displayedName;
-
-  // bool _showWhy = true;
-  // bool _showRecipes = false;
-  // bool _isUsernameUsed = false;
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   _initialise();
-  // }
-
-  // Future<void> _initialise() async {
-  //   final bool showRecipes = await PreferencesController()
-  //       .getBool(SharedPreferencesKeys.LESSON_RECIPES_DISPLAYED_DASHBOARD);
-  //   final bool showYourWhy = await PreferencesController()
-  //       .getBool(SharedPreferencesKeys.YOUR_WHY_DISPLAYED);
-  //   final bool isUsernameUsed = await PreferencesController()
-  //       .getBool(SharedPreferencesKeys.USERNAME_USED);
-
-  // setState(() {
-  //   _showWhy = showYourWhy;
-  //   _showRecipes = showRecipes;
-  //   _isUsernameUsed = isUsernameUsed;
-  // });
-  // }
-
   @override
   Widget build(BuildContext context) {
-    // _showRecipes = showLessonReicpes;
-    // _showWhy = showYourWhy;
-    // _isUsernameUsed = isUsernameUsed;
-
-    // return ChangeNotifierProvider(create: (context) => PreferencesProvider(),
     return Column(
         children: [
           Expanded(
