d47271700e09976c8a8bee61059a80430bd06e29
Vergil Santos <vergil@puttiapps.com>
2022-08-18 12:39:07 +0800

    refactor prefsProvider calls to avoid repetitive codes

--- lib/screens/settings/settings_layout.dart
+++ lib/screens/settings/settings_layout.dart
@@ -50,111 +50,62 @@ class _SettingsLayoutState extends State<SettingsLayout> {
   Map<String, bool> _preferenceOptions = {};
 
   @override
-  Widget build(BuildContext context) => Container(
-        decoration: BoxDecoration(
-          color: primaryColor,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Header(
-              title: "Account Preferences",
-              closeItem: () => Navigator.pop(context),
-              showDivider: true,
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: _preferenceOptions.length,
-                shrinkWrap: true,
-                itemBuilder: (BuildContext context, int index) => Column(
-                  children: <Widget>[
-                    Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 15, vertical: 25),
-                      child: ToggleSwitch(
-                          title: _preferenceOptions.keys.elementAt(index),
-                          value: _preferenceOptions.values.elementAt(index),
-                          onToggle: (isOn) {
-                            final key =
-                                _preferenceOptions.keys.elementAt(index);
+  Widget build(BuildContext context) {
+    PreferencesProvider prefsProvider =
+        Provider.of<PreferencesProvider>(context, listen: false);
 
-                            setState(() {
-                              _preferenceOptions.addAll({key: isOn});
-                            });
+    return Container(
+      decoration: BoxDecoration(
+        color: primaryColor,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Header(
+            title: "Account Preferences",
+            closeItem: () => Navigator.pop(context),
+            showDivider: true,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _preferenceOptions.length,
+              shrinkWrap: true,
+              itemBuilder: (BuildContext context, int index) => Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 25),
+                    child: ToggleSwitch(
+                        title: _preferenceOptions.keys.elementAt(index),
+                        value: _preferenceOptions.values.elementAt(index),
+                        onToggle: (isOn) {
+                          final key = _preferenceOptions.keys.elementAt(index);
 
-                            switch (index) {
-                              case 0:
-                                Provider.of<PreferencesProvider>(context, listen: false).saveDisplayWhy(isOn);
-                                break;
-                              case 1:
-                                Provider.of<PreferencesProvider>(context, listen: false).saveIsShowLessonRecipes(isOn);
-                                break;
-                              case 2:
-                                Provider.of<PreferencesProvider>(context, listen: false).saveIsUsernameUsed(isOn);
-                                break;
-                              default:
-                                break;
-                            }
-                          }),
-                    ),
-                    Divider(thickness: 1, height: 1, color: dividerColor),
-                  ],
-                ),
+                          setState(() {
+                            _preferenceOptions.addAll({key: isOn});
+                          });
+
+                          switch (index) {
+                            case 0:
+                              prefsProvider.saveDisplayWhy(isOn);
+                              break;
+                            case 1:
+                              prefsProvider.saveIsShowLessonRecipes(isOn);
+                              break;
+                            case 2:
+                              prefsProvider.saveIsUsernameUsed(isOn);
+                              break;
+                            default:
+                              break;
+                          }
+                        }),
+                  ),
+                  Divider(thickness: 1, height: 1, color: dividerColor),
+                ],
               ),
             ),
-
-            // Expanded(
-            //   child: LayoutBuilder(
-            //     builder: (BuildContext context, BoxConstraints constraints) {
-            //       return Container(
-            //         height: constraints.maxHeight,
-            //         child: SingleChildScrollView(
-            //           child: _isLoading
-            //               ? PcosLoadingSpinner()
-            //               : Padding(
-            //                   padding: const EdgeInsets.all(16.0),
-            //                   child: Column(
-            //                     children: [
-            //                       NotificationsPermissions(
-            //                         notificationPermissions:
-            //                             _notificationPermissions,
-            //                         requestNotificationPermission:
-            //                             _requestNotificationPermission,
-            //                       ),
-            //                       DailyReminder(
-            //                         isDailyReminderOn: _isDailyReminderOn,
-            //                         dailyReminderTimeOfDay:
-            //                             _dailyReminderTimeOfDay,
-            //                         notificationPermissions:
-            //                             _notificationPermissions,
-            //                         saveDailyReminder: _saveDailyReminder,
-            //                         showTimeDialog: _showTimeDialog,
-            //                       ),
-            //                       widget.onlyShowDailyReminder
-            //                           ? Container()
-            //                           : Column(
-            //                               children: [
-            //                                 YourWhySetting(
-            //                                     isYourWhyOn: _isYourWhyOn,
-            //                                     hasYourWhyBeenEntered:
-            //                                         _hasYourWhyBeenAnswered,
-            //                                     saveYourWhy: _saveYourWhy),
-            //                                 LessonRecipes(
-            //                                     isLessonRecipesOn:
-            //                                         _isLessonRecipesOn,
-            //                                     saveLessonRecipes:
-            //                                         _saveLessonRecipes),
-            //                               ],
-            //                             ),
-            //                     ],
-            //                   ),
-            //                 ),
-            //         ),
-            //       );
-            //     },
-            //   ),
-            // ),
-          ],
-        ),
-      );
+          ),
+        ],
+      ),
+    );
+  }
 }
