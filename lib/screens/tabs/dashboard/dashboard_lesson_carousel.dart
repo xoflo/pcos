9173005ca11efc20a6bb3d27cb4f42960a15598d
Vergil Santos <vergil@puttiapps.com>
2022-08-23 10:29:13 +0800

    remove unused imports

--- lib/screens/tabs/dashboard/dashboard_lesson_carousel.dart
+++ lib/screens/tabs/dashboard/dashboard_lesson_carousel.dart
@@ -4,7 +4,6 @@ import 'package:animations/animations.dart';
 import 'package:thepcosprotocol_app/constants/loading_status.dart';
 import 'package:thepcosprotocol_app/generated/l10n.dart';
 import 'package:thepcosprotocol_app/models/navigation/lesson_arguments.dart';
-import 'package:thepcosprotocol_app/providers/loading_status_notifier.dart';
 import 'package:thepcosprotocol_app/providers/modules_provider.dart';
 import 'package:thepcosprotocol_app/providers/preferences_provider.dart';
 import 'package:thepcosprotocol_app/screens/tabs/more/quiz.dart';
@@ -67,11 +66,11 @@ class DashboardLessonCarousel extends StatelessWidget {
                     final currentLesson =
                         modulesProvider.currentModuleLessons[index];
 
-                    final currentLessonQuiz = modulesProvider
-                        .getQuizByLessonID(currentLesson.lessonID);
+                    final currentLessonQuiz =
+                        modulesProvider.getQuizByLessonID(currentLesson.lessonID);
 
-                    final currentLessonRecipes = modulesProvider
-                        .getLessonRecipes(currentLesson.lessonID);
+                    final currentLessonRecipes =
+                       modulesProvider.getLessonRecipes(currentLesson.lessonID);
                     final isLessonComplete = currentLesson.isComplete;
 
                     int lessonRecipeDuration = 0;
--- lib/widgets/shared/loader_overlay2.dart
+++ lib/widgets/shared/loader_overlay2.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:thepcosprotocol_app/generated/l10n.dart';
 import 'package:thepcosprotocol_app/constants/loading_status.dart';
-import 'package:thepcosprotocol_app/widgets/shared/pcos_loading_spinner.dart';
-import 'package:provider/provider.dart';
-import 'package:thepcosprotocol_app/providers/modules_provider.dart';
 import 'package:thepcosprotocol_app/styles/colors.dart';
 import 'package:thepcosprotocol_app/providers/loading_status_notifier.dart';
 import 'package:thepcosprotocol_app/widgets/shared/no_results.dart';
@@ -40,7 +37,6 @@ class LoaderOverlay extends StatelessWidget {
           alignment: indicatorPosition,
           child: CircularProgressIndicator(
             valueColor: new AlwaysStoppedAnimation<Color>(backgroundColor),
-            // child: CircularProgressIndicator(),
           ),
         ),
       );
