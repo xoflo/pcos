fd37ce3b3022df3ebad74e641bd264114024ec0a
Jacob Chan <jacob@gitlab.puttiapps.com>
2022-09-20 09:25:03 +0800

    Fix onboarding/tutorial cut off issue
    https://moacreative.teamwork.com/#/tasks/36439134

--- lib/widgets/shared/carousel/base_carousel_page.dart
+++ lib/widgets/shared/carousel/base_carousel_page.dart
@@ -20,8 +20,6 @@ mixin BaseCarouselPage<T extends StatelessWidget> on StatelessWidget {
 
   int get itemsLength;
 
-  int get carouselFlex => 1;
-
   CustomPainter? get painter;
 
   final ValueNotifier<int> activePage = ValueNotifier(0);
@@ -45,81 +43,83 @@ mixin BaseCarouselPage<T extends StatelessWidget> on StatelessWidget {
     return AnnotatedRegion<SystemUiOverlayStyle>(
       value: SystemUiOverlayStyle(
         statusBarColor: primaryColor,
-        statusBarIconBrightness: Brightness.dark),
+        statusBarIconBrightness: Brightness.dark,
+      ),
       child: WillPopScope(
-      onWillPop: () => Future.value(shouldPopScope(context)),
-      child: SafeArea( 
-        child: Scaffold(
-        backgroundColor: primaryColorLight,
-        body: Stack(
-          alignment: Alignment.center,
-          children: [
-            CustomPaint(
-              painter: painter,
-              child: Container(
-                width: width,
-                height: height,
-              ),
-            ),
-            SafeArea(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Flexible(
-                    flex: carouselFlex,
-                    child: Container(
-                      child: PageView.builder(
-                        controller: controller,
-                        itemCount: itemsLength,
-                        pageSnapping: true,
-                        itemBuilder: getItemBuilder,
-                        onPageChanged: updatePageValue,
+        onWillPop: () => Future.value(shouldPopScope(context)),
+        child: SafeArea(
+          bottom: Platform.isAndroid,
+          top: Platform.isAndroid,
+          child: Scaffold(
+            backgroundColor: primaryColorLight,
+            body: Stack(
+              alignment: Alignment.center,
+              children: [
+                CustomPaint(
+                  painter: painter,
+                  child: Container(
+                    width: width,
+                    height: height,
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Flexible(
+                      flex: 2,
+                      child: Container(
+                        child: PageView.builder(
+                          controller: controller,
+                          itemCount: itemsLength,
+                          pageSnapping: true,
+                          itemBuilder: getItemBuilder,
+                          onPageChanged: updatePageValue,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 20),
-                  Flexible(
-                    child: Column(
-                      children: [
-                        indicator,
-                        ...getButtons(context),
-                      ],
+                    SizedBox(height: 20),
+                    Flexible(
+                      child: Column(
+                        children: [
+                          indicator,
+                          ...getButtons(context),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            if (showBackButton(context))
-              Positioned(
-                top: 0.0,
-                left: 0.0,
-                right: 0.0,
-                child: AppBar(
-                  systemOverlayStyle: SystemUiOverlayStyle(
-                      statusBarColor: primaryColor,
-                      statusBarIconBrightness: Brightness.dark),
-                  leading: IconButton(
-                    icon: Icon(
-                      Icons.arrow_back,
-                      color: backgroundColor,
-                    ),
-                    onPressed: () {
-                      // We update the active page so that when popping,
-                      // we remove the big ellipsis painted earlier
-                      updatePageValue(activePage.value + 1);
-
-                      Navigator.pop(context);
-                    },
-                  ),
-                  backgroundColor: Colors.transparent,
-                  elevation: 0.0,
+                    SizedBox(height: 20),
+                  ],
                 ),
-              ),
-            ],
+                if (showBackButton(context))
+                  Positioned(
+                    top: 0.0,
+                    left: 0.0,
+                    right: 0.0,
+                    child: AppBar(
+                      systemOverlayStyle: SystemUiOverlayStyle(
+                          statusBarColor: primaryColor,
+                          statusBarIconBrightness: Brightness.dark),
+                      leading: IconButton(
+                        icon: Icon(
+                          Icons.arrow_back,
+                          color: backgroundColor,
+                        ),
+                        onPressed: () {
+                          // We update the active page so that when popping,
+                          // we remove the big ellipsis painted earlier
+                          updatePageValue(activePage.value + 1);
+
+                          Navigator.pop(context);
+                        },
+                      ),
+                      backgroundColor: Colors.transparent,
+                      elevation: 0.0,
+                    ),
+                  ),
+              ],
+            ),
           ),
         ),
-      ),
       ),
     );
   }
