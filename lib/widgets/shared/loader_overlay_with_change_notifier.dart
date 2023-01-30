b8026c8aa6ac7060325d654edc077ff6df2d62a1
Chao Wang <chao@puttiapps.com>
2023-01-31 11:17:13 +1300

    Only show loading message if loading for awhile
    To avoid the jarring flashing of text if the loading is short.

--- lib/widgets/shared/loader_overlay_with_change_notifier.dart
+++ lib/widgets/shared/loader_overlay_with_change_notifier.dart
@@ -1,11 +1,13 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:thepcosprotocol_app/generated/l10n.dart';
 import 'package:thepcosprotocol_app/constants/loading_status.dart';
-import 'package:thepcosprotocol_app/styles/colors.dart';
+import 'package:thepcosprotocol_app/generated/l10n.dart';
 import 'package:thepcosprotocol_app/providers/loading_status_notifier.dart';
-import 'package:thepcosprotocol_app/widgets/shared/no_results.dart';
+import 'package:thepcosprotocol_app/styles/colors.dart';
 import 'package:thepcosprotocol_app/utils/dialog_utils.dart';
 import 'package:thepcosprotocol_app/widgets/shared/filled_button.dart';
+import 'package:thepcosprotocol_app/widgets/shared/no_results.dart';
 
 /// A Widget that makes another widget overlayed with a progress indicator on top of it.
 /// Purpose of which is to move out the logic behind the appearance/disappearance of
@@ -15,7 +17,7 @@ import 'package:thepcosprotocol_app/widgets/shared/filled_button.dart';
 /// LoadingStatusNotifier.
 /// The change notifier must have its own status attribute of type LoadingStatus and must
 /// call setLoadingStatus(fetchAndSaveDataStatus, false); each time its status changes.
-class LoaderOverlay extends StatelessWidget {
+class LoaderOverlay extends StatefulWidget {
   LoaderOverlay({
     Key? key,
     required this.child,
@@ -46,21 +48,65 @@ class LoaderOverlay extends StatelessWidget {
   final Function? retryAction;
 
   @override
-  Widget build(BuildContext context) => Stack(
+  State<LoaderOverlay> createState() => _LoaderOverlayState();
+}
+
+class _LoaderOverlayState extends State<LoaderOverlay> {
+  Timer? _sustainCheckTimer;
+  bool _isSustainedLoading = false;
+
+  @override
+  void dispose() {
+    _sustainCheckTimer?.cancel();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _checkLoadingStatus();
+
+    return Stack(
         children: [
-          child,
+          widget.child,
           _loaderOverlay(context),
         ],
       );
+  }
+
+  void _checkLoadingStatus() {
+    switch (widget.loadingStatusNotifier.loadingStatus) {
+      case LoadingStatus.loading:
+        // Only show loading message if it's been loading for longer than a second
+        _sustainCheckTimer = Timer(Duration(seconds: 1), () {
+          if (!_isSustainedLoading) {
+            setState(() {
+              _isSustainedLoading = true;
+            });
+          }
+        });
+        break;
+      default:
+        {
+          // Cancel any pending sustained timer if `LoadingStatus` is not `loading`
+          _sustainCheckTimer?.cancel();
+
+          setState(() {
+            _isSustainedLoading = false;
+          });
+        }
+        break;
+    }
+  }
 
   Widget _loaderOverlay(BuildContext context) {
-    switch (loadingStatusNotifier.loadingStatus) {
+    switch (widget.loadingStatusNotifier.loadingStatus) {
       case LoadingStatus.loading:
         return Container(
-          height: height,
-          color: overlayBackgroundColor ?? Colors.grey.withOpacity(0.5),
+          height: widget.height,
+          color: widget.overlayBackgroundColor ?? Colors.grey.withOpacity(0.5),
           child: Align(
-            alignment: indicatorPosition,
+            alignment: widget.indicatorPosition,
             child: SizedBox(
               width: 200,
               height: 110,
@@ -70,11 +116,11 @@ class LoaderOverlay extends StatelessWidget {
                     valueColor:
                         new AlwaysStoppedAnimation<Color>(backgroundColor),
                   ),
-                  if (loadingMessage.length > 0) ...[
+                  if (widget.loadingMessage.length > 0 && _isSustainedLoading) ...[
                     Padding(
                       padding: const EdgeInsets.only(top: 5.0),
                       child: Text(
-                        loadingMessage,
+                        widget.loadingMessage,
                         softWrap: true,
                         textAlign: TextAlign.center,
                         style: Theme.of(context)
@@ -91,22 +137,22 @@ class LoaderOverlay extends StatelessWidget {
         );
 
       case LoadingStatus.empty:
-        return Center(child: NoResults(message: emptyMessage));
+        return Center(child: NoResults(message: widget.emptyMessage));
       case LoadingStatus.success:
         return Container();
       case LoadingStatus.failed:
-        if (isDisplayErrorAsAlert) {
+        if (widget.isDisplayErrorAsAlert) {
           Widget retryButton = FilledButton(
             margin: EdgeInsets.zero,
             text: "Retry",
             foregroundColor: Colors.white,
             backgroundColor: backgroundColor,
             onPressed: () {
-              if (isErrorDialogDismissible) {
-                if (retryAction != null) {
-                  Function.apply(retryAction!, positionalParams, namedParams);
+              if (widget.isErrorDialogDismissible) {
+                if (widget.retryAction != null) {
+                  Function.apply(widget.retryAction!, widget.positionalParams, widget.namedParams);
                 }
-                loadingStatusNotifier.setLoadingStatus(
+                widget.loadingStatusNotifier.setLoadingStatus(
                     LoadingStatus.success, true);
               }
             },
