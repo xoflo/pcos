acd0e16ffea30aba18493ad27c94cc4f252bc329
Chao Wang <chao@puttiapps.com>
2023-01-31 10:08:58 +1300

    Issue JWT token to show verified user on Zendesk console
    The JWT token (that includes username and `external_id`) allows the
    Zendesk admins to know who the user is in a chat session.

---
+++ lib/constants/zendesk_jwt_key.dart
@@ -0,0 +1,5 @@
+// https://developer.zendesk.com/documentation/zendesk-web-widget-sdks/sdks/web/enabling_auth_visitors/
+// App ID and JWT hash key (algo: HS256) for the Ovie team
+const ZendeskAppId = "app_63cf31cd99c2520109f5a824";
+const ZendeskJWTKey = "OnuXDgikN-WadRlv1uirD5pxQsAvxIHzL6Gm56zFq4UWmagkmwZlYZTaV1wJxzhdwAX3pJl3rIpl2WgpiYheMw";
+
--- lib/screens/zendesk_web_view.dart
+++ lib/screens/zendesk_web_view.dart
@@ -1,5 +1,7 @@
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:thepcosprotocol_app/constants/zendesk_jwt_key.dart';
 
 import '../controllers/authentication_controller.dart';
 import '../styles/colors.dart';
@@ -28,8 +30,8 @@ class _ZendeskWebViewState extends State<ZendeskWebView> {
   void getUserDetails() async {
     final authenticationController = AuthenticationController();
 
-    _userId = await authenticationController.getUserId() ?? "unknown-user-id";
-    _userName = await authenticationController.getUsername() ?? "unknown-user-name";
+    _userId = await authenticationController.getUserId();
+    _userName = await authenticationController.getUsername();
   }
 
   @override
@@ -47,17 +49,44 @@ class _ZendeskWebViewState extends State<ZendeskWebView> {
             Expanded(
               child: InAppWebView(
                 initialFile: "assets/html/zendesk.html",
-                onLoadStop: (controller, url) {
-                  // Tag the chat session with the username and userId to help
-                  // the Ovie team manage Zendesk sessions.
-                  //
-                  // https://developer.zendesk.com/api-reference/widget/chat-api/#chataddtags
-                  controller.evaluateJavascript(
-                      source: "zE('webWidget', 'chat:addTags', ['$_userName', '$_userId'])");
+                onLoadStop: (controller, url) async {
+                  // Add a bit of a delay, so the app only attempts to login
+                  // after the Zendesk web widget finishes loading.
+                  await Future.delayed(const Duration(seconds: 2));
+
+                  final token = issueZendeskJWT();
+                  if (token != null) {
+                    // https://developer.zendesk.com/documentation/zendesk-web-widget-sdks/sdks/web/sdk_api_reference/#authentication
+                    controller.evaluateJavascript(source: """
+                      zE('messenger', 'loginUser', function (callback) {
+                        callback('$token');
+                        });
+                        """);
+                  }
                 },
               ),
             ),
           ],
         )));
+  }
+
+  // TODO: A better/more secure appraoch is to have the server issue the JWT
+  String? issueZendeskJWT() {
+    // https://developer.zendesk.com/documentation/zendesk-web-widget-sdks/sdks/web/enabling_auth_visitors/
+    if (_userName != null && _userId != null) {
+      final jwt = JWT({
+        "scope": "user",
+        "name": _userName,
+        "external_id": _userId,
+      }, header: {
+        "alg": "HS256",
+        "typ": "JWT",
+        "kid": ZendeskAppId,
+      });
+
+      return jwt.sign(SecretKey(ZendeskJWTKey));
+    }
+
+    return null;
   }
 }
