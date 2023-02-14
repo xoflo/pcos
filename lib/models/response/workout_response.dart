3260bc0c6cc8da5ab9729bf17ef96b344e1b0571
Vergil Santos <vergil@puttiapps.com>
2023-02-14 12:22:11 +0800

    updated example response

--- lib/models/response/workout_response.dart
+++ lib/models/response/workout_response.dart
@@ -17,31 +17,132 @@ class WorkoutResponse {
   }
 }
 
-// TODO: change to the actual workout response from API
 //Example
 /*
 {
   "status": "OK",
   "message": "",
   "info": "",
-  "payload":
-   [
-      {
-        "recipeId": 22,
-        "title": "2 Minute Mayo",
-        "description": "",
-        "slug": "two-minute-mayo",
-        "thumbnail": "images/2_Minute_Mayo.jpg",
-        "ingredients": "<ul>\r\n\t<li>2 eggs</li>\r\n\t<li>3 tbsp lemon juice</li>\r\n\t<li>1 tsp Dijon mustard</li>\r\n\t<li>&frac12; tsp salt</li>\r\n\t<li>White pepper</li>\r\n\t<li>300ml avocado oil or olive oil&nbsp;</li>\r\n</ul>",
-        "method": "<ol>\r\n\t<li style=\"list-style-type:decimal\">Place all ingredients in a narrow bowl or jug, then blitz with a hand blender until you get a thick and creamy mayo. If you&rsquo;d like a thicker consistency then use an extra 60ml oil or use an extra 60ml hot water for a lighter consistency.&nbsp;</li>\r\n\t<li style=\"list-style-type:decimal\">Store for up to 2 weeks in the refrigerator.</li>\r\n</ol>",
-        "tips": "<p>Makes one jar.</p>",
-        "difficulty": 0,
-        "servings": 1,
-        "duration": 120000000,
-        "isFavorite":false,
-        "lastUpdatedUTC": "2020-11-17T04:14:36.3",
-        "dateCreatedUTC": "2020-09-30T21:26:16.477"
-      }
+  "payload": [
+    {
+      "adminName": "",
+      "adminNotes": "",
+      "workoutID": 1,
+      "title": "BeginnerMove1",
+      "description": "Agreatplacetostartyourmovementjourney, usingourbodyweightforresistance!",
+      "tags": "Bodyweight, Pregnancyfriendly",
+      "minsToComplete": 45,
+      "orderIndex": 10,
+      "imageUrl": "https: //asset.cloudinary.com/dbee2ldxn/ea5cf91d579ea5be6d217892caac4255",
+      "isFavorite": false,
+      "isComplete": false,
+      "exercises": [
+        {
+          "exerciseID": 1,
+          "title": "Tricepdips",
+          "description": "<p>Legsbentforlessweightorstraight/evenraisedforahardervariation.</p>\r\n<p>Keeparmsapproxhipwidth.</p>\r\n<p>Engagetriceps.</p>\r\n<p>Focusonkeepingelbowsinastraightlineratherthansplayingout.</p>",
+          "imageUrl": "",
+          "mediaUrl": "https: //1693712952.rsc.cdn77.org/122448/63e4536d6e5a3605108634playlist-1080.m3u8",
+          "equipmentRequired": "",
+          "tags": "",
+          "setsMinimum": 3,
+          "setsMaximum": 3,
+          "repsMinimum": 10,
+          "repsMaximum": 10,
+          "secsBetweenSets": 0,
+          "isFavorite": false,
+          "lastUpdatedUTC": "2023-02-09T03: 24: 53.573",
+          "dateCreatedUTC": "2023-02-09T03: 24: 53.573"
+        },
+        {
+          "exerciseID": 2,
+          "title": "Wallpushups",
+          "description": "lorenipsum",
+          "imageUrl": "",
+          "mediaUrl": "https: //1693712952.rsc.cdn77.org/122448/63e468cb85fb9846744788playlist-1080.m3u8",
+          "equipmentRequired": "",
+          "tags": "",
+          "setsMinimum": 3,
+          "setsMaximum": 3,
+          "repsMinimum": 10,
+          "repsMaximum": 10,
+          "secsBetweenSets": 0,
+          "isFavorite": false,
+          "lastUpdatedUTC": "2023-02-09T03: 25: 29.51",
+          "dateCreatedUTC": "2023-02-09T03: 25: 29.51"
+        },
+        {
+          "exerciseID": 3,
+          "title": "Pullaparts-BANDED",
+          "description": "lorenipsum",
+          "imageUrl": "",
+          "mediaUrl": "https: //s3.amazonaws.com/spotlightr-output/122448/63e4654fa4872440483248playlist-1080.m3u8",
+          "equipmentRequired": "",
+          "tags": "",
+          "setsMinimum": 3,
+          "setsMaximum": 3,
+          "repsMinimum": 10,
+          "repsMaximum": 10,
+          "secsBetweenSets": 0,
+          "isFavorite": false,
+          "lastUpdatedUTC": "2023-02-09T03: 26: 07.697",
+          "dateCreatedUTC": "2023-02-09T03: 26: 07.697"
+        },
+        {
+          "exerciseID": 4,
+          "title": "Stepups, bodyweight",
+          "description": "<p>Makesureyourworkinglegisdoingallofthework.</p>\r\n<p>Ifyou’resteppingupwithyourleftleg, don’tpushoffthegroundwithyourrightone.</p>\r\n<p>Whenyouloweryourleftfootbacktotheground, doitundercontrol.</p>",
+          "imageUrl": "",
+          "mediaUrl": "https: //1693712952.rsc.cdn77.org/122448/63e4537f078c8002243608playlist-1080.m3u8",
+          "equipmentRequired": "",
+          "tags": "",
+          "setsMinimum": 3,
+          "setsMaximum": 3,
+          "repsMinimum": 12,
+          "repsMaximum": 12,
+          "secsBetweenSets": 0,
+          "isFavorite": false,
+          "lastUpdatedUTC": "2023-02-09T03: 27: 28.183",
+          "dateCreatedUTC": "2023-02-09T03: 27: 28.183"
+        },
+        {
+          "exerciseID": 5,
+          "title": "Romaniandeadlift, banded",
+          "description": "lorenipsum",
+          "imageUrl": "",
+          "mediaUrl": "https: //1693712952.rsc.cdn77.org/122448/63e4655f0e459227628035playlist-1080.m3u8",
+          "equipmentRequired": "",
+          "tags": "",
+          "setsMinimum": 3,
+          "setsMaximum": 3,
+          "repsMinimum": 10,
+          "repsMaximum": 10,
+          "secsBetweenSets": 0,
+          "isFavorite": false,
+          "lastUpdatedUTC": "2023-02-09T03: 27: 50.42",
+          "dateCreatedUTC": "2023-02-09T03: 27: 50.42"
+        },
+        {
+          "exerciseID": 6,
+          "title": "Squat, normalstance",
+          "description": "<p>Standwithyourfeethip-widthapartandyourtoespointingforward.</p>\r\n<p>Loweryourbodyasifyouweresittingbackintoachair, keepingyourbackstraightandyourchestlifted.</p>\r\n<p>Godownasfarasyoucanwhilekeepingyourheelsflatontheground.</p>\r\n<p>Pushbackupthroughyourheelstoreturntothestartingposition.</p>\r\n<p>Repeatforthedesirednumberofrepetitions.</p>\r\n<p>Makesuretokeepyourkneesoveryouranklesandnotextendpastyourtoesasyousquat.</p>\r\n<p>Keepyourweightinyourheels, andengageyourcoretomaintainbalanceandstability.</p>",
+          "imageUrl": "",
+          "mediaUrl": "https: //1693712952.rsc.cdn77.org/122448/63e4536b95c64782467747playlist-1080.m3u8",
+          "equipmentRequired": "",
+          "tags": "",
+          "setsMinimum": 3,
+          "setsMaximum": 3,
+          "repsMinimum": 10,
+          "repsMaximum": 10,
+          "secsBetweenSets": 0,
+          "isFavorite": false,
+          "lastUpdatedUTC": "2023-02-09T03: 30: 20.1",
+          "dateCreatedUTC": "2023-02-09T03: 30: 20.1"
+        }
+      ],
+      "lastUpdatedUTC": "1900-01-01T00: 00: 00",
+      "dateCreatedUTC": "2023-02-09T03: 21: 49.023"
+    }
   ]
 }
 */
