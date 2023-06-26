ce440a027cd045789e340cb9427df9bbadc3873b
Vergil Santos <vergil@puttiapps.com>
2023-06-27 03:17:06 +0800

    avoid crash when running in Android 9

--- android/app/src/main/kotlin/com/thepcosnutritionist/thepcosprotocol_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/thepcosnutritionist/thepcosprotocol_app/MainActivity.kt
@@ -8,7 +8,9 @@ import io.flutter.embedding.android.FlutterActivity
 class MainActivity : FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     // Aligns the Flutter view vertically with the window.
-    WindowCompat.setDecorFitsSystemWindows(getWindow(), false)
+    if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+      WindowCompat.setDecorFitsSystemWindows(getWindow(), false)
+    }
     
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
       // Disable the Android splash screen fade out animation to avoid
