b4a5d6ce5850cf6e33c85502a53f0d050ca0fa8b
Vergil Santos <vergil@puttiapps.com>
2023-06-30 07:52:30 +0800

    remove hardcoded users for getstream.io

--- lib/screens/community/app_user.dart
+++ lib/screens/community/app_user.dart
@@ -1,41 +1,15 @@
 //ignore: public_member_api_docs
 class AppUser {
   //ignore: public_member_api_docs
-  const AppUser.sahil()
-      : id = 'sahil-kumar',
-        name = 'Sahil Kumar',
-        token =
-            '''eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoic2FoaWwta3VtYXIifQ.dcQEvOX7cfbvIrmpSkL7Luvzok7uqPYhbcJ740CTTq4''';
 
   //ignore: public_member_api_docs
-  const AppUser.sacha()
-      : id = 'sacha-arbonel',
-        name = 'Sacha Arbonel',
-        token =
-            '''eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoic2FjaGEtYXJib25lbCJ9.522mIrFIIKSoDxGEzH1kpFqlEitT1mrWqNGf9VemgHc''';
+  late final String id;
 
   //ignore: public_member_api_docs
-  const AppUser.nash()
-      : id = 'neevash-ramdial',
-        name = 'Neevash Ramdial',
-        token =
-            '''eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibmVldmFzaC1yYW1kaWFsIn0.RMuwITrfC3PkctN9p90oIcpVQOQRVSHE2emSLD5pbK0'''; //ignore: public_member_api_docs
+  late final String name;
 
   //ignore: public_member_api_docs
-  const AppUser.reuben()
-      : id = 'groovin',
-        name = 'Reuben Turner',
-        token =
-            '''eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZ3Jvb3ZpbiJ9.sQQozIjgtxUkgX9QalOM2UYhzQL6UvscsOvrgV1LYsM''';
-
-  //ignore: public_member_api_docs
-  final String id;
-
-  //ignore: public_member_api_docs
-  final String name;
-
-  //ignore: public_member_api_docs
-  final String token;
+  late final String token;
 
   //ignore: public_member_api_docs
   Map<String, Object> get data {
@@ -48,10 +22,4 @@ class AppUser {
   }
 }
 
-//ignore: public_member_api_docs
-const appUsers = [
-  AppUser.sahil(),
-  AppUser.sacha(),
-  AppUser.nash(),
-  AppUser.reuben(),
-];
+const appUsers = [];
