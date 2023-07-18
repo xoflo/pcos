f8243ac2bbb1b9d7098622730b1374e0f8f8cd78
Nicks Villanueva <nico@puttiapps.com>
2023-07-18 21:51:34 +0800

    UI Updates for Profile Screen

--- lib/screens/community/list_activity_item.dart
+++ lib/screens/community/list_activity_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:stream_feed_flutter_core/stream_feed_flutter_core.dart';
+import 'package:thepcosprotocol_app/styles/colors.dart';
 
 import 'comments_page.dart';
 
@@ -9,16 +10,18 @@ import 'comments_page.dart';
 ///
 /// Enables the current [User] to like the activity, and view comments.
 class ListActivityItem extends StatelessWidget {
-  const ListActivityItem({
-    Key? key,
-    required this.user,
-    required this.activity,
-    required this.feedGroup,
-  }) : super(key: key);
+  const ListActivityItem(
+      {Key? key,
+      required this.user,
+      required this.activity,
+      required this.feedGroup,
+      required this.isLandscapeImage})
+      : super(key: key);
 
   final EnrichedActivity activity;
   final String feedGroup;
   final String user;
+  final bool isLandscapeImage;
 
   @override
   Widget build(BuildContext context) {
@@ -26,41 +29,50 @@ class ListActivityItem extends StatelessWidget {
     final reactionCounts = activity.reactionCounts;
     final ownReactions = activity.ownReactions;
     final isLikedByUser = (ownReactions?['like']?.length ?? 0) > 0;
-    return ListTile(
-      leading: CircleAvatar(
-        backgroundImage: NetworkImage(''), //actor.profileImage
-      ),
-      title: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-        child: Row(
-          children: [
-            Text(user), //actor.fullName
-            const SizedBox(width: 8),
-            Text(
-              '@' + user,
-              style: Theme.of(context).textTheme.caption,
-            ),
-          ],
-        ),
-      ),
-      subtitle: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      child: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Text('${activity.object}'),
+          SizedBox(height: 10),
+          Row(
+            children: [
+              SizedBox(
+                width: 15,
+              ),
+              CircleAvatar(
+                backgroundImage: NetworkImage(''),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Text(user),
+            ],
           ),
-          if (attachments != null && attachments.isNotEmpty)
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: Image.network(attachments[0].url),
-            ),
+          Column(
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              if (attachments != null && attachments.isNotEmpty)
+                Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: isLandscapeImage ? 300 : 500,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(attachments[0].url),
+                      ),
+                    ))
+            ],
+          ),
           Row(
             children: [
               Row(
                 children: [
+                  SizedBox(
+                    width: 10,
+                  ),
                   IconButton(
-                    iconSize: 16,
+                    iconSize: 30,
                     onPressed: () {
                       if (isLikedByUser) {
                         context.feedBloc.onRemoveReaction(
@@ -76,24 +88,22 @@ class ListActivityItem extends StatelessWidget {
                             feedGroup: feedGroup);
                       }
                     },
+                    color: isLikedByUser ? backgroundColor : Colors.grey,
                     icon: isLikedByUser
-                        ? const Icon(Icons.favorite_rounded)
-                        : const Icon(Icons.favorite_outline),
+                        ? const Icon(Icons.favorite)
+                        : const Icon(Icons.favorite_border),
                   ),
-                  if (reactionCounts?['like'] != null)
-                    Text(
-                      '${reactionCounts?['like']}',
-                      style: Theme.of(context).textTheme.caption,
-                    )
                 ],
               ),
-              const SizedBox(width: 16),
               Row(
                 children: [
                   IconButton(
-                    iconSize: 16,
+                    iconSize: 25,
                     onPressed: () => _navigateToCommentPage(context),
-                    icon: const Icon(Icons.mode_comment_outlined),
+                    icon: const Icon(
+                      Icons.message_outlined,
+                      color: Colors.grey,
+                    ),
                   ),
                   if (reactionCounts?['comment'] != null)
                     Text(
@@ -103,22 +113,53 @@ class ListActivityItem extends StatelessWidget {
                 ],
               )
             ],
+          ),
+          if (reactionCounts?['like'] != null)
+            Row(children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 20, top: 0, right: 20, bottom: 0),
+                child: Text(
+                  _displayNumberOfLikes(reactionCounts?['like']),
+                  style: Theme.of(context).textTheme.caption,
+                  textAlign: TextAlign.left,
+                ),
+              )
+            ]),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 20, top: 10, right: 20, bottom: 10),
+                child: Text(
+                  '${activity.object}',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(fontSize: 12),
+                ),
+              )
+            ],
           )
         ],
       ),
-      onTap: () {
-        _navigateToCommentPage(context);
-      },
     );
   }
 
   void _navigateToCommentPage(BuildContext context) {
     Navigator.of(context).push(
       MaterialPageRoute<void>(
-        builder: (BuildContext context) => CommentsPage(
-          activity: activity,
-        ),
-      ),
+          builder: (BuildContext context) => CommentsPage(
+                activity: activity,
+              ),
+          fullscreenDialog: true),
     );
+  }
+
+  String _displayNumberOfLikes(int? numberOfLikes) {
+    if (numberOfLikes != null) {
+      String likesText = numberOfLikes > 1 ? "Likes" : "Like";
+      return '$numberOfLikes' + " " + likesText;
+    } else {
+      return "";
+    }
   }
 }
